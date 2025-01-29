0e0d4ffa171d3b5c0580ba742d1f26cae2622e99
Thales Gelinger <thalesgelinger@gmail.com>
2025-01-29 10:57:04 -0300

    feat: update setup script

--- static/sh/flake.nix
+++ static/sh/flake.nix
@@ -14,7 +14,7 @@
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages =
-        [ pkgs.kitty
+        [ pkgs.wezterm
           pkgs.neovim
           pkgs.tmux
           pkgs.asdf-vm
